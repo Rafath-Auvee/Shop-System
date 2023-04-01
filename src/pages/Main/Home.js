475654f44743e80d26452ec72056f7f330f3c11c
Rafath-Auvee <rbza98@outlook.com>
2023-04-02 01:08:20 +0600

    backend refresh every 2 mins

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
 
+const apiKey = process.env.URI;
+console.log("🚀 ~ file: Home.js:5 ~ apiKey:", apiKey);
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]); 
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch(`https://shop-system-react-redux-backend.onrender.com/products`)
       .then((res) => res.json())
       .then((data) => setProducts(data.data));
   }, []);
@@ -13,8 +15,8 @@ const Home = () => {
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
         <button
           className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
         >
@@ -27,7 +29,7 @@ const Home = () => {
           Intel
         </button>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
         {products.map((product) => (
           <ProductCard key={product.model} product={product} />
         ))}
