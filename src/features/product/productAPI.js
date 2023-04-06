808aa76d12cf3df9b0ae177e4188a38697cf3336
Rafath-Auvee <rbza98@outlook.com>
2023-04-06 09:46:20 +0600

    adding product successful

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -2,8 +2,11 @@ import axios from "../../utils/axios.config.js";
 
 export const fetchProducts = async () => {
   const response = await axios.get("/products");
-  console.log(response.data.data);
-  const data = response.data.data
+  // console.log(response.data.data);
+  const data = response.data.data;
   return data;
 };
 
+export const postProduct = async (productData) => {
+  const response = await axios.post("/product", productData);
+};
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchProducts } from "./productAPI.js";
+import { fetchProducts, postProduct } from "./productAPI.js";
 
 const initialState = {
   products: [],
   isLoading: false,
+  postSuccess: false,
   isError: false,
   errorMessage: "",
 };
@@ -15,25 +16,49 @@ export const getProducts = createAsyncThunk(
     return products;
   }
 );
+export const addProducts = createAsyncThunk(
+  "products/addProducts",
+  async (data) => {
+    const products = postProduct(data);
+    return products;
+  }
+);
 
 const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getProducts.pending, (state) => {
-      state.isLoading = true;
-      state.products = [];
-    });
-    builder.addCase(getProducts.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.products = action.payload;
-    });
-    builder.addCase(getProducts.rejected, (state, action) => {
-      state.isLoading = false;
-      state.isError = true;
-      state.errorMessage = action.error.message;
-    });
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+        state.products = [];
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.errorMessage = action.error.message;
+      })
+      .addCase(addProducts.pending, (state) => {
+        state.isLoading = true;
+        state.postSuccess = false;
+        state.isError = false;
+      })
+      .addCase(addProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.postSuccess = true;
+      })
+      .addCase(addProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.errorMessage = action.error.message;
+        state.postSuccess = false;
+        state.products = [];
+      });
   },
 });
 
